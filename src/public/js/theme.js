5cf580a26450c4ca956243a95c65b88e3860ded7
oitsjustjose <stovallj1995@gmail.com>
2020-07-13 08:35:09 -0500

    " -> ', auto day/night theme

--- src/public/js/theme.js
+++ src/public/js/theme.js
@@ -1,44 +1,69 @@
+let currentDisplayMode = null;
+let themeWatcher = null;
+
 const updateDisplayModeIcons = () => {
-    let el = document.querySelector(".color-select");
-    if (currentDisplayMode == "dark") {
+    let el = document.querySelector('.color-select');
+    if (currentDisplayMode == 'dark') {
         el.innerHTML = '🌑';
-    } else {
+    } else if (currentDisplayMode == 'light') {
         el.innerHTML = '☀️';
+    } else {
+        el.innerHTML = '🕐';
     }
 };
 
 const toggleDisplayMode = () => {
-    if (currentDisplayMode == "dark") {
-        window.localStorage.setItem("displayMode", "light");
-        currentDisplayMode = "light";
-        document.body.classList.remove("inverted");
+    if (themeWatcher) {
+        clearInterval(themeWatcher);
+    }
+
+    if (currentDisplayMode == 'dark') {
+        window.localStorage.setItem('displayMode', 'light');
+        currentDisplayMode = 'light';
+        document.body.classList.remove('inverted');
+    } else if (currentDisplayMode == 'light') {
+        window.localStorage.setItem('displayMode', 'auto');
+        currentDisplayMode = 'auto';
+        initThemeWatcher();
     } else {
-        window.localStorage.setItem("displayMode", "dark");
-        currentDisplayMode = "dark";
-        document.body.classList.add("inverted");
+        window.localStorage.setItem('displayMode', 'dark');
+        currentDisplayMode = 'dark';
+        document.body.classList.add('inverted');
     }
     updateDisplayModeIcons();
 };
 
+const initThemeWatcher = () => {
+    themeWatcher = setInterval(() => {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.body.classList.add('inverted');
+        } else {
+            document.body.classList.remove('inverted');
+        }
+    }, 1000);
+};
+
 window.addEventListener('load', () => {
-    document.querySelector(".color-select").addEventListener('click', (evt) => {
+    document.querySelector('.color-select').addEventListener('click', (evt) => {
         toggleDisplayMode();
     });
 
-    if (window.localStorage.getItem("displayMode") == null) {
+    if (window.localStorage.getItem('displayMode') == null) {
         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            window.localStorage.setItem("displayMode", "dark");
+            window.localStorage.setItem('displayMode', 'dark');
         } else {
-            window.localStorage.setItem("displayMode", "light");
+            window.localStorage.setItem('displayMode', 'light');
         }
     }
 
-    currentDisplayMode = window.localStorage.getItem("displayMode");
+    currentDisplayMode = window.localStorage.getItem('displayMode');
 
-    if (currentDisplayMode == "dark") {
-        document.body.classList.add("inverted");
+    if (currentDisplayMode == 'dark') {
+        document.body.classList.add('inverted');
+    } else if (currentDisplayMode == 'light') {
+        document.body.classList.remove('inverted');
     } else {
-        document.body.classList.remove("inverted");
+        initThemeWatcher();
     }
 
     updateDisplayModeIcons();
